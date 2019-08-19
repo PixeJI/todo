bbce7782d51c3e743c5408f396105ecbdf83e68f
Pixel <andrey.nazarov007@gmail.com>
2019-08-19 19:11:10 +0300

    Updates

---
+++ precache-manifest.377fab024c4dd7fd0f7190828f60bbd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f953648f309cec7467e2888959b8cf86",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "7d9a0dca4b8621dd22a7",
+    "url": "/todo/static/css/main.7776c153.chunk.css"
+  },
+  {
+    "revision": "ac4663307a4cc8208001",
+    "url": "/todo/static/js/2.7506cabe.chunk.js"
+  },
+  {
+    "revision": "7d9a0dca4b8621dd22a7",
+    "url": "/todo/static/js/main.75268c78.chunk.js"
+  },
+  {
+    "revision": "263ea585d3801eaba5dc",
+    "url": "/todo/static/js/runtime~main.447bd38d.js"
+  }
+]);
